bd83fc63edef05811031d8799f7b5116e6fbd940
richardhob <richardhob@gmail.com>
2020-11-20 17:28:53 -0800

    Update license to mention ME (the maintainer here)

--- dwf/lowlevel.py
+++ dwf/lowlevel.py
@@ -1,29 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-===========================================
-Digilent's DWF library wrapper for python.
-===========================================
-
-Copyright notice
-================
-
-Copyright (c) 2016 MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
-
-This software is released under the MIT License.
-http://opensource.org/licenses/mit-license.php
-
-
-Supported platforms
-===================
-
-* Python 2.6, 2.7 or Python 3.3 or above
-* Windows, OSX, or Linux with Digilent's Waveforms 2015 or lator
-
-This code is tested with Waveforms SDK, October 12, 2015 version.
-'''
-
 import sys
 import os
 from ctypes import *
